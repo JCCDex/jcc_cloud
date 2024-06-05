384d95b074ad8629f85babd5f8bacf523ba926bc
Libre <gin.musheng@gmail.com>
2024-06-05 13:04:58 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -32,10 +32,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 60,
-      functions: 60,
-      lines: 60,
-      statements: 60
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
     }
   },
 
