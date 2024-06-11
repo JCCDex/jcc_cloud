16cad283710aa531c7b7af3c99b595019b2ace25
Libre <gin.musheng@gmail.com>
2024-06-11 15:10:43 +0800

    add explorer wallet's APIs (#2)

--- src/explorer.ts
+++ src/explorer.ts
@@ -1,8 +1,23 @@
 import { CloudError } from "./error";
 import fetch from "./fetch";
-import { IFetchBalancesOptions, IFetchBalancesResponse, IResponse } from "./types";
+import { IResponse, OrderType, TradeType, PageSize,
+  IFetchBalancesOptions, IFetchBalancesResponse,
+  IFetchOffersOptions, IFetchOffersResponse,
+  IFetchHistoryOrdersOptions, IFetchHistoryOrdersResponse,
+  IFetchIssuedTokensOptions, IFetchIssuedTokensResponse,
+  IFetchHistoryFeesOptions, IFetchHistoryFeesResponse
+
+} from "./types";
 
 export default class JCCDexExplorer {
+  readonly timeOffset = 946684800000;
+
+  public orderType = OrderType;
+
+  public tradeType = TradeType;
+
+  public pageSize = PageSize;
+
   private baseUrl: string;
 
   constructor(baseUrl: string) {
@@ -48,4 +63,109 @@ export default class JCCDexExplorer {
 
     return { code, msg, data: { balances } };
   }
+
+  public async fetchOffers(options: IFetchOffersOptions): Promise<IFetchOffersResponse> {
+    const res: IResponse = await fetch({
+      method: "get",
+      baseURL: this.baseUrl,
+      url: "/wallet/offer/" + options.uuid,
+      params: {
+        w: options.address,
+        p: options.page || 0,
+        s: options.size || this.pageSize.TWENTY,
+        c: options.coinPair || "",
+        bs: options.buyOrSell || this.tradeType.ALL
+      }
+    });
+    const { code, msg, data } = res;
+    if (!this.isSuccess(code)) {
+      throw new CloudError(code, msg);
+    }
+
+    const offers = (data.list as any[] || []);
+    offers.forEach(offer => {
+      offer.time = offer.time * 1000 + this.timeOffset;
+    });
+    return { code, msg, data: { offers } };
+  }
+
+  public async fetchHistoryOrders(options: IFetchHistoryOrdersOptions): Promise<IFetchHistoryOrdersResponse> {
+    const res: IResponse = await fetch({
+      method: "get",
+      baseURL: this.baseUrl,
+      url: "/wallet/trans/" + options.uuid,
+      params: {
+        w: options.address,
+        p: options.page || 0,
+        s: options.size || this.pageSize.TWENTY,
+        b: options.beginTime || "",
+        e: options.endTime || "",
+        t: options.type || this.orderType.ALL,
+        c: options.coinPair || "",
+        bs: options.buyOrSell || this.tradeType.ALL
+      }
+    });
+    const { code, msg, data } = res;
+    if (!this.isSuccess(code)) {
+      throw new CloudError(code, msg);
+    }
+
+    const historOrders = (data.list as any[] || []);
+    historOrders.forEach(order => {
+      order.time = order.time * 1000 + this.timeOffset;
+    });
+    return { code, msg, data: { historOrders } };
+  }
+
+  public async fetchIssuedTokens(options: IFetchIssuedTokensOptions): Promise<IFetchIssuedTokensResponse> {
+    const res: IResponse = await fetch({
+      method: "get",
+      baseURL: this.baseUrl,
+      url: "/wallet/fingate_tokenlist/" + options.uuid,
+      params: {
+        w: options.address
+      }
+    });
+    const { code, msg, data } = res;
+    if (!this.isSuccess(code)) {
+      throw new CloudError(code, msg);
+    }
+
+    const tokens = (data[options.address] as any[] || []);
+    return { code, msg, data: { tokens } };
+  }
+
+  public async fetchHistoryFees(options: IFetchHistoryFeesOptions): Promise<IFetchHistoryFeesResponse> {
+    const res: IResponse = await fetch({
+      method: "get",
+      baseURL: this.baseUrl,
+      url: "/wallet/trans/fee/" + options.uuid,
+      params: {
+        w: options.address,
+        p: options.page || 0,
+        s: options.size || this.pageSize.TWENTY,
+        b: options.beginTime || "",
+        e: options.endTime || "",
+        c: options.tokenAndIssuer || "",
+        t: "Fee",
+      }
+    });
+    const { code, msg, data } = res;
+    if (!this.isSuccess(code)) {
+      throw new CloudError(code, msg);
+    }
+
+    const fees = (data.list as any[] || []);
+
+    fees.forEach(fee => {
+      const [currency, issuer] = fee.currency.split("_");
+      fee.currency = currency;
+      fee.issuer = issuer || "";
+      fee.time = fee.time * 1000 + this.timeOffset;
+    });
+    return { code, msg, data: { fees } };
+  }
+
 }
+
+
--- test/explorer.spec.ts
+++ test/explorer.spec.ts
@@ -8,6 +8,7 @@ describe("test explorer", () => {
   const baseUrl = "https://swtcscan.jccdex.cn";
   const explorer = new JCCDexExplorer(baseUrl);
   const stub = sandbox.stub(fetch, "default");
+  const timeOffset = explorer.timeOffset;
 
   describe("test fetchBalances", () => {
     afterEach(() => {
@@ -88,4 +89,451 @@ describe("test explorer", () => {
       });
     });
   });
+
+  describe("test fetchOffers", () => {
+    afterEach(() => {
+      sandbox.reset();
+    });
+
+    test("should throw error when response is not success", async () => {
+      stub.resolves({
+        code: "-1",
+        msg: "error"
+      });
+      try {
+        await explorer.fetchOffers({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        });
+      } catch (error) {
+        expect(error instanceof CloudError).toEqual(true);
+        expect(error.code).toEqual("-1");
+        expect(error.message).toEqual("error");
+      }
+      await expect(
+        explorer.fetchOffers({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        })
+      ).rejects.toThrow(new CloudError("-1", "error"));
+    });
+
+    test("should return offers", async () => {
+      stub.resolves({
+        code: "0",
+        msg: "",
+        data: {
+          list: [
+            {
+              time: 100,
+              past: 1000,
+              hash: "hash",
+              block: 1000,
+              flag: 1,
+              takerGets: {
+                currency: "SWTC",
+                value: "100000000",
+                issuer: ""
+              },
+              takerPays: {
+                currency: "CNY",
+                value: "100000000",
+                issuer: "jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or"
+              },
+              seq: 1000
+            }
+          ]
+        }
+      });
+      const res = await explorer.fetchOffers({
+        uuid: "jGa9J9TkqtBc",
+        address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi",
+        size: explorer.pageSize.TEN,
+        buyOrSell: explorer.tradeType.ALL
+      });
+
+      expect(
+        stub.calledOnceWithExactly({
+          method: "get",
+          baseURL: "https://swtcscan.jccdex.cn",
+          url: "/wallet/offer/jGa9J9TkqtBc",
+          params: { 
+            w: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi",
+            p: 0,
+            s: 10,
+            c: "",
+            bs: ""
+          }
+        })
+      ).toEqual(true);
+
+      expect(res).toEqual({
+        code: "0",
+        msg: "",
+        data: {
+          offers: [
+            {
+              time: 100000 + timeOffset,
+              past: 1000,
+              hash: "hash",
+              block: 1000,
+              flag: 1,
+              takerGets: {
+                currency: "SWTC",
+                value: "100000000",
+                issuer: ""
+              },
+              takerPays: {
+                currency: "CNY",
+                value: "100000000",
+                issuer: "jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or"
+              },
+              seq: 1000
+            }
+          ]
+        }
+      });
+    });
+  });
+
+  describe("test fetchHistoryOrders", () => {
+    afterEach(() => {
+      sandbox.reset();
+    });
+
+    test("should throw error when response is not success", async () => {
+      stub.resolves({
+        code: "-1",
+        msg: "error"
+      });
+      try {
+        await explorer.fetchHistoryOrders({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        });
+      } catch (error) {
+        expect(error instanceof CloudError).toEqual(true);
+        expect(error.code).toEqual("-1");
+        expect(error.message).toEqual("error");
+      }
+      await expect(
+        explorer.fetchHistoryOrders({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        })
+      ).rejects.toThrow(new CloudError("-1", "error"));
+    });
+
+    test("should return historyOrders", async () => {
+      stub.resolves({
+        code: "0",
+        msg: "",
+        data: {
+          list: [
+            {
+              type: 'OfferCreate',
+              time: 100,
+              hash: 'A5FAED341A6292447F130056A68ACB2155AE0C37287D04CF12DD17CCE1C0AA2B',
+              block: 28818929,
+              fee: 0.001,
+              success: 'tesSUCCESS',
+              seq: 1382376,
+              takerGets: {
+                currency: 'JUSDT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '20.028'
+              },
+              takerPays: {
+                currency: 'JUNI',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '2'
+              },
+              takerGetsFact: {
+                currency: 'JUSDT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '20.028'
+              },
+              takerPaysFact: {
+                currency: 'JUNI',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '2'
+              },
+              past: 103,
+              flag: 1
+            }
+          ]
+        }
+      });
+      const res = await explorer.fetchHistoryOrders({
+        uuid: "jGa9J9TkqtBc",
+        address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi",
+        size: explorer.pageSize.FIFTY,
+        type: explorer.orderType.OFFERCREATE,
+        buyOrSell: explorer.tradeType.BUY
+      });
+
+      expect(
+        stub.calledOnceWithExactly({
+          method: "get",
+          baseURL: "https://swtcscan.jccdex.cn",
+          url: "/wallet/trans/jGa9J9TkqtBc",
+          params: { 
+            w: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi",
+            p: 0,
+            s: 50,
+            b: "",
+            e: "",
+            t: "OfferCreate",
+            c: "",
+            bs: 1
+          }
+        })
+      ).toEqual(true);
+
+      expect(res).toEqual({
+        code: "0",
+        msg: "",
+        data: {
+          historOrders: [
+            {
+              type: explorer.orderType.OFFERCREATE,
+              time: 100000 + timeOffset,
+              hash: 'A5FAED341A6292447F130056A68ACB2155AE0C37287D04CF12DD17CCE1C0AA2B',
+              block: 28818929,
+              fee: 0.001,
+              success: 'tesSUCCESS',
+              seq: 1382376,
+              takerGets: {
+                currency: 'JUSDT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '20.028'
+              },
+              takerPays: {
+                currency: 'JUNI',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '2'
+              },
+              takerGetsFact: {
+                currency: 'JUSDT',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '20.028'
+              },
+              takerPaysFact: {
+                currency: 'JUNI',
+                issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or',
+                value: '2'
+              },
+              past: 103,
+              flag: 1
+            }
+          ]
+        }
+      });
+    });
+  });
+
+  describe("test fetchIssuedTokens", () => {
+    afterEach(() => {
+      sandbox.reset();
+    });
+
+    test("should throw error when response is not success", async () => {
+      stub.resolves({
+        code: "-1",
+        msg: "error"
+      });
+      try {
+        await explorer.fetchIssuedTokens({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        });
+      } catch (error) {
+        expect(error instanceof CloudError).toEqual(true);
+        expect(error.code).toEqual("-1");
+        expect(error.message).toEqual("error");
+      }
+      await expect(
+        explorer.fetchIssuedTokens({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        })
+      ).rejects.toThrow(new CloudError("-1", "error"));
+    });
+
+    test("should return tokens", async () => {
+      stub.resolves({
+        code: "0",
+        msg: "",
+        "data": {
+          "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi": [
+            {
+              "currency": "CNY",
+              "issuer": "jGwru5JHQ2y9iHqF4LwqWjrQinyBqoxzd5"
+            },
+            {
+              "currency": "EUR",
+              "issuer": "jBhNRiWfJAd8mfiVTCcHCueJAL9pJhN5Nf"
+            },
+            {
+              "currency": "8000000000000000000000007F92BA0351D9547B",
+              "issuer": "jBhNRiWfJAd8mfiVTCcHCueJAL9pJhN5Nf"
+            }
+          ]
+        }
+      });
+      const res = await explorer.fetchIssuedTokens({
+        uuid: "jGa9J9TkqtBc",
+        address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+      });
+
+      expect(
+        stub.calledOnceWithExactly({
+          method: "get",
+          baseURL: "https://swtcscan.jccdex.cn",
+          url: "/wallet/fingate_tokenlist/jGa9J9TkqtBc",
+          params: { w: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi" }
+        })
+      ).toEqual(true);
+
+      expect(res).toEqual({
+        code: "0",
+        msg: "",
+        data: {
+          tokens: [
+            { 
+              currency: 'CNY',
+              issuer: 'jGwru5JHQ2y9iHqF4LwqWjrQinyBqoxzd5'
+            },
+            { 
+              currency: 'EUR',
+              issuer: 'jBhNRiWfJAd8mfiVTCcHCueJAL9pJhN5Nf'
+            },
+            {
+              currency: '8000000000000000000000007F92BA0351D9547B',
+              issuer: 'jBhNRiWfJAd8mfiVTCcHCueJAL9pJhN5Nf'
+            }
+          ]
+        }
+      });
+    });
+  });
+
+  describe("test fetchHistoryFees", () => {
+    afterEach(() => {
+      sandbox.reset();
+    });
+
+    test("should throw error when response is not success", async () => {
+      stub.resolves({
+        code: "-1",
+        msg: "error"
+      });
+      try {
+        await explorer.fetchHistoryFees({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        });
+      } catch (error) {
+        expect(error instanceof CloudError).toEqual(true);
+        expect(error.code).toEqual("-1");
+        expect(error.message).toEqual("error");
+      }
+      await expect(
+        explorer.fetchHistoryFees({
+          uuid: "jGa9J9TkqtBc",
+          address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi"
+        })
+      ).rejects.toThrow(new CloudError("-1", "error"));
+    });
+
+    test("should return fees", async () => {
+      stub.resolves({
+        code: "0",
+        msg: "",
+        "data": {
+          "list": [
+            {
+              "type": "Fee",
+              "block": 28809485,
+              "time": 100,
+              "currency": "JUSDT_jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or",
+              "value": "0.0019904444765127",
+              "den": 1000,
+              "num": 2,
+              "platform": "jDXCeSHSpZ9LiX6ihckWaYDeDt5hFrdTto",
+              "hash": "B8181A68686D8AFC276DDABD132C55760255D0096DCFC2AD81EC58E029421AF2"
+            },
+            {
+              "type": "Fee",
+              "block": 28809485,
+              "time": 200,
+              "currency": "JUSDT_jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or",
+              "value": "0.0092025487034873",
+              "den": 1000,
+              "num": 2,
+              "platform": "jDXCeSHSpZ9LiX6ihckWaYDeDt5hFrdTto",
+              "hash": "91DA75B6E07A2CF09C919A0B834000044A99F0840ACF1BFD0823FE59A8D186E3"
+            }
+          ]
+        }
+      });
+      const res = await explorer.fetchHistoryFees({
+        uuid: "jGa9J9TkqtBc",
+        address: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi",
+        size: explorer.pageSize.HUNDRED,
+        tokenAndIssuer: "JUSDT_jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or"
+      });
+
+      expect(
+        stub.calledOnceWithExactly({
+          method: "get",
+          baseURL: "https://swtcscan.jccdex.cn",
+          url: "/wallet/trans/fee/jGa9J9TkqtBc",
+          params: { 
+            w: "j4rmEZiaTdXBkgzXPdsu1JRBf5onngqfUi", 
+            p: 0,
+            s: 100,
+            b: "",
+            e: "",
+            c: "JUSDT_jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or",
+            t: "Fee",
+          }
+        })
+      ).toEqual(true);
+
+      expect(res).toEqual({
+        code: "0",
+        msg: "",
+        data: {
+          fees: [
+            {
+              type: 'Fee',
+              block: 28809485,
+              time: 100000 + timeOffset,
+              currency: 'JUSDT',
+              value: '0.0019904444765127',
+              den: 1000,
+              num: 2,
+              platform: 'jDXCeSHSpZ9LiX6ihckWaYDeDt5hFrdTto',
+              hash: 'B8181A68686D8AFC276DDABD132C55760255D0096DCFC2AD81EC58E029421AF2',
+              issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            },
+            {
+              type: 'Fee',
+              block: 28809485,
+              time: 200000 + timeOffset,
+              currency: 'JUSDT',
+              value: '0.0092025487034873',
+              den: 1000,
+              num: 2,
+              platform: 'jDXCeSHSpZ9LiX6ihckWaYDeDt5hFrdTto',
+              hash: '91DA75B6E07A2CF09C919A0B834000044A99F0840ACF1BFD0823FE59A8D186E3',
+              issuer: 'jGa9J9TkqtBcUoHe2zqhVFFbgUVED6o9or'
+            }
+          ]
+        }
+      });
+    });
+  });
+
 });
